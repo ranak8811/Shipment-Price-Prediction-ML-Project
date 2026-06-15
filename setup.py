9eddda9f41f32faa36e56dd28ece740ff76692a5
Anwar_Hossain <ranakrphone@gmail.com>
2026-06-15 21:07:03 +0600

    project setup and packages installation done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="Anwar",
+    author_email="ranakrphone@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
