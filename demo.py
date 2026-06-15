dc5a38ac2941c3b0ecf6ff108e7b7ea3d907e06e
Anwar_Hossain <ranakrphone@gmail.com>
2026-06-15 23:48:26 +0600

    utility for yaml helper methods added

--- demo.py
+++ demo.py
@@ -1,11 +1,18 @@
 from shipment.logger import logging
 from shipment.exception import shippingException
 import sys
+from shipment.utils.main_utils import MainUtils
 
 # logging.info('this is checking 2')
 
-try:
-    a = 1 / 0
+# try:
+#     a = 1 / 0
 
-except Exception as e:
-    raise shippingException(e, sys) from e
+# except Exception as e:
+#     raise shippingException(e, sys) from e
+
+obj = MainUtils()
+
+data = obj.read_yaml_file('config/model.yaml')
+
+print(data)
